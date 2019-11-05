9765e347c66db9922282cb4e082970b2bf7cfeb9
Yuval Cohen <yuval.email@gmail.com>
2019-11-05 18:21:05 +1100

    Implemented Move

--- toy_robot.hpp
+++ toy_robot.hpp
@@ -15,6 +15,8 @@ namespace toy_robot {
 	/**
 	* @class ToyRobot
 	* Toy Robot class
+	* @template parameter X grid size (>= 1)
+	* @template parameter Y grid size (>= 1)
 	*/
 	template <uint32_t X, uint32_t Y>
 	class ToyRobot : public ToyRobotBase {
@@ -30,6 +32,14 @@ namespace toy_robot {
 		*/
 		~ToyRobot() = default;
 
+		/**
+		* Place
+		* @brief place toy robot on grid
+		* @param x coordinate
+		* @param y coordinate
+		* @param face north/south/east/west
+		* @return true - place successful, false - otherwise
+		*/
 		bool Place(uint32_t x, uint32_t y, Face face) override {
 			if (x < X && y < Y) {
 				x_ = x;
@@ -42,8 +52,47 @@ namespace toy_robot {
 			return false;
 		}
 
+		/**
+		* Place
+		* @brief move toy robot one unit forward in the direction it is currently facing
+		* @return false - robot not placed, true - otherwise
+		*/
 		bool Move() override {
-			return false;
+			if (!placed_) {
+				return false;
+			}
+
+			switch (face_) {
+			case Face::kNorth:
+				if (y_ + 1 < Y) {
+					y_++;
+				}
+				break;
+
+			case Face::kSouth:
+				if (y_ > 0) {
+					y_--;
+				}
+				break;
+
+			case Face::kEast:
+				if (x_ + 1 < X) {
+					x_++;
+				}
+				break;
+
+			case Face::kWest:
+				if (x_ > 0) {
+					x_--;
+				}
+				break;
+
+			default:
+				placed_ = false;
+				return false;
+
+			}
+			return true;
 		}
 
 		bool Left() override {
