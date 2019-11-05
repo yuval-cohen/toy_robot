27bba0670f4a9d9a7d27e8f926ecf569280ae1aa
Yuval Cohen <yuval.email@gmail.com>
2019-11-05 19:07:23 +1100

    Implemented second unit test for toy robot (#b from instructions)

--- test_toy_robot/run_test.cpp
+++ test_toy_robot/run_test.cpp
@@ -34,11 +34,32 @@ void test__PLACE_0_0_NORTH__MOVE__REPORT() {
 	}
 }
 
+void test__PLACE_0_0_NORTH__LEFT__REPORT() {
+	ToyRobot<5, 5> toy_robot;
+
+	cout << "PLACE 0,0,NORTH" << endl;
+	toy_robot.Place(0, 0, Face::kNorth);
+	cout << "LEFT" << endl;
+	toy_robot.Left();
+	cout << "REPORT" << endl;
+	uint32_t x;
+	uint32_t y;
+	Face face;
+	if (toy_robot.Report(&x, &y, &face) && (x == 0) && (y == 0) && (face == Face::kWest)) {
+		cout << "Output: " << x << "," << y << ",WEST" << endl;
+		cout << "Test #2 [OK]\n\n";
+	}
+	else {
+		cout << "Test #2 [FAIL]\n\n";
+	}
+}
+
 int main() {
 
 	cout << endl << "Toy Robot Unit Tests" << endl;
 
 	test__PLACE_0_0_NORTH__MOVE__REPORT();
+	test__PLACE_0_0_NORTH__LEFT__REPORT();
 
 	return 0;
 }
