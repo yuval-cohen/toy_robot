fcc7a5f4b708093a33a403fd91d63c9460cc1bd4
Yuval Cohen <yuval.email@gmail.com>
2019-11-05 19:16:49 +1100

    Implemented third unit test for toy robot (#c from instructions)

--- test_toy_robot/run_test.cpp
+++ test_toy_robot/run_test.cpp
@@ -14,7 +14,13 @@ using namespace std;
 using toy_robot::ToyRobot;
 using toy_robot::Face;
 
-void test__PLACE_0_0_NORTH__MOVE__REPORT() {
+/*
+ * PLACE 0,0,NORTH
+ * MOVE
+ * REPORT
+ * Output: 0,1,NORTH
+*/
+void test__1() {
 	ToyRobot<5,5> toy_robot;
 
 	cout << "PLACE 0,0,NORTH" << endl;
@@ -34,7 +40,13 @@ void test__PLACE_0_0_NORTH__MOVE__REPORT() {
 	}
 }
 
-void test__PLACE_0_0_NORTH__LEFT__REPORT() {
+/*
+ * PLACE 0,0,NORTH
+ * LEFT
+ * REPORT
+ * Output: 0,0,WEST
+*/
+void test__2() {
 	ToyRobot<5, 5> toy_robot;
 
 	cout << "PLACE 0,0,NORTH" << endl;
@@ -53,13 +65,48 @@ void test__PLACE_0_0_NORTH__LEFT__REPORT() {
 		cout << "Test #2 [FAIL]\n\n";
 	}
 }
+/*
+ * PLACE 1, 2, EAST
+ * MOVE
+ * MOVE
+ * LEFT
+ * MOVE
+ * REPORT
+ * Output : 3, 3, NORTH
+*/
+void test__3() {
+	ToyRobot<5, 5> toy_robot;
+
+	cout << "PLACE 1, 2, EAST" << endl;
+	toy_robot.Place(1, 2, Face::kEast);
+	cout << "MOVE" << endl;
+	toy_robot.Move();
+	cout << "MOVE" << endl;
+	toy_robot.Move();
+	cout << "LEFT" << endl;
+	toy_robot.Left();
+	cout << "MOVE" << endl;
+	toy_robot.Move();
+	cout << "REPORT" << endl;
+	uint32_t x;
+	uint32_t y;
+	Face face;
+	if (toy_robot.Report(&x, &y, &face) && (x == 3) && (y == 3) && (face == Face::kNorth)) {
+		cout << "Output: " << x << "," << y << ",NORTH" << endl;
+		cout << "Test #3 [OK]\n\n";
+	}
+	else {
+		cout << "Test #3 [FAIL]\n\n";
+	}
+}
 
 int main() {
 
 	cout << endl << "Toy Robot Unit Tests" << endl;
 
-	test__PLACE_0_0_NORTH__MOVE__REPORT();
-	test__PLACE_0_0_NORTH__LEFT__REPORT();
+	test__1();
+	test__2();
+	test__3();
 
 	return 0;
 }
