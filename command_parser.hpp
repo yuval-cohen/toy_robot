c37eb5088e4b78fe68c72ff72c4fa6f175535387
Yuval Cohen <yuval.email@gmail.com>
2019-11-05 17:38:06 +1100

    just indentation

--- command_parser.hpp
+++ command_parser.hpp
@@ -13,39 +13,39 @@
 namespace toy_robot {
 namespace command_parser {
 
-			/**
-			* @class CommandParser
-			* Command Parser class
-			*/
-			class CommandParser : public CommandParserBase {
-			public:
-				/**
-				* Constructor
-				*/
-				CommandParser() = default;
+	/**
+	* @class CommandParser
+	* Command Parser class
+	*/
+	class CommandParser : public CommandParserBase {
+	public:
+		/**
+		* Constructor
+		*/
+		CommandParser() = default;
 
-				/**
-				* Destructor
-				*/
-				~CommandParser() = default;
+		/**
+		* Destructor
+		*/
+		~CommandParser() = default;
 
-				/**
-				* @brief Parse command string
-				* @param input command string (null terminated)
-				* @param output command data
-				* @return true - command successfully parsed; false - otherwise
-				*/
-				bool ParseCommand(const char *command_str, Command *command) override;
+		/**
+		* @brief Parse command string
+		* @param input command string (null terminated)
+		* @param output command data
+		* @return true - command successfully parsed; false - otherwise
+		*/
+		bool ParseCommand(const char *command_str, Command *command) override;
 
-			private:
-				bool SkipSpaces(const char *command_str, size_t cmd_str_len, size_t *current_parse_idx);
-				bool ParseCommandStrWithParams(const char *command_str, size_t command_str_len, size_t *current_parse_idx, Command *command);
-				bool ParseU32Numeric(const char *command_str, size_t *current_parse_idx, uint32_t *n);
-				bool ParseParamSeparator(const char *command_str, size_t command_str_len, size_t *current_parse_idx);
-				bool ParseFaceString(const char *command_str, size_t command_str_len, size_t *current_parse_idx, Face *face);
+	private:
+		bool SkipSpaces(const char *command_str, size_t cmd_str_len, size_t *current_parse_idx);
+		bool ParseCommandStrWithParams(const char *command_str, size_t command_str_len, size_t *current_parse_idx, Command *command);
+		bool ParseU32Numeric(const char *command_str, size_t *current_parse_idx, uint32_t *n);
+		bool ParseParamSeparator(const char *command_str, size_t command_str_len, size_t *current_parse_idx);
+		bool ParseFaceString(const char *command_str, size_t command_str_len, size_t *current_parse_idx, Face *face);
 
-				bool StrToU32(const char *start, uint32_t *u32);
-			};
+		bool StrToU32(const char *start, uint32_t *u32);
+	};
 
 }  // namespace command_parser
 }  // namespace toy_robot
