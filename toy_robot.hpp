d0148fdc70ded00fce8e3209b074660b328f2c05
Yuval Cohen <yuval.email@gmail.com>
2019-11-05 18:27:01 +1100

    Implemented Right

--- toy_robot.hpp
+++ toy_robot.hpp
@@ -130,8 +130,39 @@ namespace toy_robot {
 			return true;
 		}
 
+		/**
+		* Right
+		* @brief rotate the robot 90 degrees to the right (without changing the position of the robot)
+		* @return false - robot not placed, true - otherwise
+		*/
 		bool Right() override {
-			return false;
+			if (!placed_) {
+				return false;
+			}
+
+			switch (face_) {
+			case Face::kNorth:
+				face_ = Face::kEast;
+				break;
+
+			case Face::kSouth:
+				face_ = Face::kWest;
+				break;
+
+			case Face::kEast:
+				face_ = Face::kSouth;
+				break;
+
+			case Face::kWest:
+				face_ = Face::kNorth;
+				break;
+
+			default:
+				placed_ = false;
+				return false;
+			}
+
+			return true;
 		}
 
 		bool Report(uint32_t *x, uint32_t *y, Face *face) override {
