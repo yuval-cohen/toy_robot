81b605ae8af2f463da4929690ed43821bf7e09df
Yuval Cohen <yuval.email@gmail.com>
2019-11-05 14:27:30 +1100

    Further implemented command parser Added command_str header

--- command_parser.cpp
+++ command_parser.cpp
@@ -6,6 +6,7 @@
 //! @copyright Yuval Cohen 2019
 //!
 
+#include "command_str.hpp"
 #include "command_parser.hpp"
 #include <cstring>
 
@@ -49,6 +50,64 @@ namespace command_parser {
 	}
 
 	bool CommandParser::ParseCommand(const char *command_str, size_t command_str_len, size_t *current_parse_idx, Command *command) {
+		if (*current_parse_idx < command_str_len) {
+			auto place_command_str_len = strlen(strings::kPlaceCommandStr);
+			auto move_command_str_len = strlen(strings::kMoveCommandStr);
+			auto left_command_str_len = strlen(strings::kLeftCommandStr);
+			auto right_command_str_len = strlen(strings::kRightCommandStr);
+			auto report_command_str_len = strlen(strings::kReportCommandStr);
+
+			if (strncmp(command_str + *current_parse_idx, strings::kPlaceCommandStr,
+				place_command_str_len) == 0) {
+				*current_parse_idx += place_command_str_len;
+
+				// TODO: parse parameters
+				command->id = CommandId::kPlace;
+				command->params_type = CommandParamsType::kTwoNumericOneFace;
+				command->params.two_u32_numeric_one_face_param.x = 0;
+				command->params.two_u32_numeric_one_face_param.y = 0;
+				command->params.two_u32_numeric_one_face_param.face = Face::kNorth;
+
+				return true;
+			}
+			else if (strncmp(command_str + *current_parse_idx, strings::kMoveCommandStr,
+				move_command_str_len) == 0) {
+				*current_parse_idx += move_command_str_len;
+
+				command->id = CommandId::kMove;
+				command->params_type = CommandParamsType::kNone;
+
+				return true;
+			}
+			else if (strncmp(command_str + *current_parse_idx, strings::kLeftCommandStr,
+				left_command_str_len) == 0) {
+				*current_parse_idx += left_command_str_len;
+
+				command->id = CommandId::kLeft;
+				command->params_type = CommandParamsType::kNone;
+
+				return true;
+			}
+			else if (strncmp(command_str + *current_parse_idx, strings::kRightCommandStr,
+				right_command_str_len) == 0) {
+				*current_parse_idx += right_command_str_len;
+
+				command->id = CommandId::kRight;
+				command->params_type = CommandParamsType::kNone;
+
+				return true;
+			}
+			else if (strncmp(command_str + *current_parse_idx, strings::kReportCommandStr,
+				report_command_str_len) == 0) {
+				*current_parse_idx += report_command_str_len;
+
+				command->id = CommandId::kReport;
+				command->params_type = CommandParamsType::kNone;
+
+				return true;
+			}
+		}
+
 		return false;
 	}
 
---
+++ command_str.hpp
@@ -0,0 +1,27 @@
+//!
+//! @file command_str.hpp
+//!
+//! @brief command strings header
+//!
+//! @copyright Yuval Cohen 2019
+//!
+
+#pragma once
+
+#include "command_defs.hpp"
+
+namespace toy_robot {
+namespace command_parser {
+namespace strings {
+
+	static constexpr const char *kPlaceCommandStr = "PLACE";
+	static constexpr const char *kMoveCommandStr = "MOVE";
+	static constexpr const char *kLeftCommandStr = "LEFT";
+	static constexpr const char *kRightCommandStr = "RIGHT";
+	static constexpr const char *kReportCommandStr = "REPORT";
+
+	static constexpr const char *kParamsSeparator = ",";
+
+}  // namespace strings
+}  // namespace command_parser
+}  // namespace toy_robot
